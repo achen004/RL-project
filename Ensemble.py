9ca3f87e09b1bb814395b2778731c5281012eade
adeet1 <adeet.patel@gmail.com>
2023-04-24 15:05:38 -0400

    Change Ensemble.py to take in command-line arguments

--- Ensemble.py
+++ Ensemble.py
@@ -2,47 +2,56 @@ from ta import add_all_ta_features
 import pandas as pd
 import torch
 from MyCustomEnv import MyCustomEnv
+from parse_args import parse_args
 from stable_baselines3 import PPO, A2C, DQN
-from stable_baselines3.common.vec_env import DummyVecEnv
 from collections import Counter
 
 torch.manual_seed(0)
 
-df = pd.read_csv("SPY_prices_2y_1h.csv")
-print("df =", df.shape)
+if __name__ == "__main__":
+    # Parse command-line arguments
+    args = parse_args()
 
-df2 = add_all_ta_features(df, open='Open', high='High', low='Low', close='Close', volume='Volume', fillna=True)
-print(df2.columns)
-N = df2.shape[0]
+    # Get S&P 500 data from Yahoo Finance
+    df = pd.read_csv(args.stock)
+    print("df =", df.shape)
 
-env_window_size = 5
-env = MyCustomEnv(df2, window_size=env_window_size, frame_bound=(env_window_size, N))#, MAX_SHARES=1000)
-obs = env.reset()
-# Train the models
-ppo_model = PPO("MlpPolicy", env, verbose=0)
-ppo_model.learn(total_timesteps=1000)
+    # Engineer financial indicators using the method imported above from the "TA" library
+    df2 = add_all_ta_features(df, open='Open', high='High', low='Low', close='Close', volume='Volume', fillna=True)
+    N = df2.shape[0]
 
-obs = env.reset()
-a2c_model = A2C("MlpPolicy", env, verbose=0)
-a2c_model.learn(total_timesteps=1000)
+    # Initialize an environment for the agent to execute trades
+    env = MyCustomEnv(df2,
+                      window_size=args.window_size,
+                      frame_bound=(args.window_size, N),
+                      MAX_SHARES=args.max_shares)
 
-obs = env.reset()
-dqn_model = DQN("MlpPolicy", env, verbose=0)
-dqn_model.learn(total_timesteps=1000)
+    # Train the models
+    obs = env.reset()
+    ppo_model = PPO("MlpPolicy", env, verbose=0)
+    ppo_model.learn(total_timesteps=1000)
 
-# Reset the environment
-env = MyCustomEnv(df2, window_size=env_window_size, frame_bound=(env_window_size, N))
-obs = env.reset()
+    obs = env.reset()
+    a2c_model = A2C("MlpPolicy", env, verbose=0)
+    a2c_model.learn(total_timesteps=1000)
 
-# Majority vote
-for _ in range(N - env_window_size):
-    ppo_action, _ = ppo_model.predict(obs)
-    a2c_action, _ = a2c_model.predict(obs)
-    dqn_action, _ = dqn_model.predict(obs)
+    obs = env.reset()
+    dqn_model = DQN("MlpPolicy", env, verbose=0)
+    dqn_model.learn(total_timesteps=1000)
 
-    actions = [int(ppo_action), int(a2c_action), int(dqn_action)]
-    majority_action = Counter(actions).most_common(1)[0][0]
-    
-    obs, rewards, dones, info = env.step(majority_action)
-    
-env.render_all()
+    # Reset the environment
+    env.reset()
+
+    # Majority vote
+    done = False
+    while not done:
+        ppo_action, _ = ppo_model.predict(obs)
+        a2c_action, _ = a2c_model.predict(obs)
+        dqn_action, _ = dqn_model.predict(obs)
+
+        actions = [int(ppo_action), int(a2c_action), int(dqn_action)]
+        majority_action = Counter(actions).most_common(1)[0][0]
+        
+        obs, reward, done, info = env.step(majority_action)
+        
+    env.render_all()
--- main.py
+++ main.py
@@ -2,37 +2,10 @@ from ta import add_all_ta_features
 import pandas as pd
 import torch
 from MyCustomEnv import MyCustomEnv
+from parse_args import parse_args
 from Agent import Agent
-import argparse
 
 torch.manual_seed(0)
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Train a reinforcement learning agent to trade a stock.")
-
-    # The path to the CSV file containing the prices for the stock we want to trade
-    parser.add_argument("--stock", type=str, default="SPY_prices_2y_1h.csv")
-
-    # Number of days of past stock prices for the agent to look at when it's deciding whether to buy/sell/hold
-    parser.add_argument("--window-size", type=int, default=50)
-
-    # Number of epochs to train the agent
-    parser.add_argument("--epochs", type=int, default=100)
-    
-    # Number of hidden nodes in the policy function (neural network) that the agent learns
-    # As a rule of thumb, this should be 2 or 3 times the window size
-    parser.add_argument("--hidden-nodes", type=int, default=200)
-
-    # Maximum number of shares we allow the agent to own at any given time
-    parser.add_argument("--max-shares", type=int, default=100)
-
-    # Learning rate for the policy gradient algorithm
-    parser.add_argument("--learning-rate", type=float, default=1e-3)
-
-    # Whether or not to use the learning rate scheduler
-    parser.add_argument("--use-lr-scheduler", type=bool, default=True)
-
-    return parser.parse_args()
 
 if __name__ == "__main__":
     # Parse command-line arguments
@@ -52,7 +25,7 @@ if __name__ == "__main__":
                       frame_bound=(args.window_size, N),
                       MAX_SHARES=args.max_shares)
 
-    # Initialize and train an agent on this environment by updating the policy function
+    # Initialize and train our custom agent on this environment by updating the policy function
     agent = Agent(action_space_dim=env.action_space.n,
                   observation_space_dim=env.observation_space.shape,
                   learning_rate=args.learning_rate,
