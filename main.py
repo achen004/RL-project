eae5023f0838bb857ecba3f7edc7c679858f92dd
adeet1 <adeet.patel@gmail.com>
2023-04-24 13:15:48 -0400

    Convert hard-coded values into command-line arguments

--- main.py
+++ main.py
@@ -3,28 +3,49 @@ import pandas as pd
 import torch
 from MyCustomEnv import MyCustomEnv
 from Agent import Agent
+import argparse
 
 torch.manual_seed(0)
 
-# Get S&P 500 data from Yahoo Finance
-#df = pd.read_csv("data/ADBE.csv")
-df = pd.read_csv("SPY_prices_2y_1h.csv")
-print("df =", df.shape)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a reinforcement learning agent to trade a stock.")
 
-# Engineer financial indicators using the method imported above from the "TA" library
-df2 = add_all_ta_features(df, open='Open', high='High', low='Low', close='Close', volume='Volume', fillna=True)
-#print(df2.columns)
-N = df2.shape[0]
+    # The path to the CSV file containing the prices for the stock we want to trade
+    parser.add_argument("--stock", type=str)
+    
+    # Number of days of past stock prices for the agent to look at when it's deciding whether to buy/sell/hold
+    parser.add_argument("--window-size", type=int)
 
-# Initialize an environment for the agent to execute trades
-env_window_size = 5
-env = MyCustomEnv(df2, window_size=env_window_size, frame_bound=(env_window_size, N))
+    # Number of epochs to train the agent
+    parser.add_argument("--epochs", type=int)
+    
+    # Number of hidden nodes in the policy function (neural network) that the agent learns
+    parser.add_argument("--hidden-nodes", type=int)
+    
+    return parser.parse_args()
 
-# Initialize and train an agent on this environment by updating the policy function
-agent = Agent(action_space_dim=env.action_space.n,
-              observation_space_dim=env.observation_space.shape,
-              learning_rate=1e-4) #TODO adjust learning_rate; maybe annealize it 
+if __name__ == "__main__":
+    # Parse command-line arguments
+    args = parse_args()
 
-agent.train(env, n_epochs=10)
+    # Get S&P 500 data from Yahoo Finance
+    df = pd.read_csv(args.stock)
+    print("df =", df.shape)
 
-env.render_all()
+    # Engineer financial indicators using the method imported above from the "TA" library
+    df2 = add_all_ta_features(df, open='Open', high='High', low='Low', close='Close', volume='Volume', fillna=True)
+    N = df2.shape[0]
+
+    # Initialize an environment for the agent to execute trades
+    env = MyCustomEnv(df2, window_size=args.window_size, frame_bound=(args.window_size, N))
+
+    # Initialize and train an agent on this environment by updating the policy function
+    agent = Agent(action_space_dim=env.action_space.n,
+                  observation_space_dim=env.observation_space.shape,
+                  learning_rate=1e-4,
+                  num_hidden_nodes=args.hidden_nodes)
+
+    agent.train(env, n_epochs=args.epochs)
+
+    # Output results
+    env.render_all()
