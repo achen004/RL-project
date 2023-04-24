0d04f683c39f4f801eea24d8363426015ba7135a
adeet1 <adeet.patel@gmail.com>
2023-04-24 11:34:08 -0400

    Get multiple tickers' data, not just the SPY ETF

--- yfinance_scraper.py
+++ yfinance_scraper.py
@@ -1,12 +1,25 @@
 # Script for scraping data from Yahoo Finance
 import yfinance as yf
+import pandas as pd
 
-SPY = yf.Ticker("SPY")
+def get_data(ticker, period, interval):
+    ticker = yf.Ticker(ticker)
+    data = ticker.history(period=period, interval=interval)
+    return data
 
-period = "1y"
-interval = "1h"
+stocks = pd.read_csv("spy_constituents.csv")
+tickers = stocks[stocks["Sector"] == "Information Technology"]["Symbol"].tolist()
 
-data = SPY.history(period=period, interval=interval, start="2021-04-14")
-print(data.head())
+success_count = 0
+for tick in tickers:
+    period = "1y"
+    interval = "1h"
+    try:
+        data = get_data(tick, period, interval)
+        print(f"Got data for {tick} with shape {data.shape}")
+        data.to_csv(f"data/{tick}.csv")
+        success_count += 1
+    except:
+        print(f"Error with {tick}")
 
-data.to_csv("SPY_prices_{}_{}.csv".format(period, interval))
+print(f"{success_count} tickers retrieved successfully")
